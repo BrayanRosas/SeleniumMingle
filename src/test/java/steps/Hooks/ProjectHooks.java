fe7a6b7366f27ca2f0e65bc2b94582ef7c70c856
BrayanRosas <brayan_rosas@hotmail.com>
2015-11-29 23:53:43 -0400

    Delete for each scenario ,improving pending

---
+++ src/test/java/steps/Hooks/ProjectHooks.java
@@ -0,0 +1,29 @@
+package steps.Hooks;
+
+import cucumber.api.java.After;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BrayanRosas
+ * Date: 11/28/15
+ * Time: 3:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectHooks {
+
+    /**
+     * Delete projects created
+     */
+    @After(value = "@Users", order = 999)
+    public void afterUsersScenarios() {
+
+
+    }
+
+
+
+
+
+
+
+}
